4b102d1814499c694d152c2e40724b0c72dc601f
Muski <143282825+muskitma@users.noreply.github.com>
2025-12-14 23:27:50 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/minalight/main.go
@@ -0,0 +1,23 @@
+// cmd/minalight/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "minalight/internal/minalight"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := minalight.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
